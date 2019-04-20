d3ed9d86ac286e5c2257c8e921a73f1d615c23d7
kthatoto <kthatoto@gmail.com>
2019-04-20 17:11:01 +0900

    Remove default resources

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
 Vue.use(Router)
 
@@ -10,17 +9,9 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: function () {
-        return import(/* webpackChunkName: "about" */ './views/About.vue')
+      name: 'Index',
+      component () {
+        return import('./views/Index.vue')
       }
     }
   ]
