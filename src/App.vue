8d27c7d071476b61f2fc9ccb26245cde49b01e9b
kthatoto <kthatoto@gmail.com>
2019-04-20 17:41:19 +0900

    Add screen

--- src/App.vue
+++ src/App.vue
@@ -4,4 +4,12 @@
 </template>
 
 <style lang="stylus">
+*
+  margin: 0
+  padding: 0
+  box-sizing: border-box
+  list-style: none
+  text-decoration: none
+a, a:visited
+  color: inherit
 </style>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,7 +1,23 @@
 <template lang="pug">
 .index
-  h1 Super Heisei Mario
+  .game
 </template>
 
 <style lang="stylus" scoped>
+.index
+  position: fixed
+  height: 100vh
+  width: 100vw
+  min-width: 800px
+  position: relative
+  .game
+    width: 800px
+    height: 600px
+    border: 1px solid black
+    position: absolute
+    top: 0
+    left: 0
+    right: 0
+    bottom: 0
+    margin: auto
 </style>
