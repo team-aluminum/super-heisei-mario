4d0b3945a05a5f25bc806a1d76b1caa61045ca93
kthatoto <kthatoto@gmail.com>
2019-04-29 23:59:36 +0900

    Move creature

--- src/components/creatures/Goomba.vue
+++ src/components/creatures/Goomba.vue
@@ -4,11 +4,12 @@
 
 <script>
 import { mapGetters } from 'vuex'
+import constants from '@/constants'
 export default {
   props: ['data'],
   computed: {
     selfStyle () {
-      const left = this.data.position.x - this.player.position.current.x
+      const left = this.data.position.x - this.player.position.current.x - this.position.x
       return Object.assign({
         width: this.data.size.width + 'px',
         height: this.data.size.height + 'px',
@@ -20,6 +21,24 @@ export default {
       player: 'getPlayer'
     })
   },
+  data () {
+    return {
+      position: {
+        x: 0,
+        y: 0
+      },
+      timer: 0
+    }
+  },
+  beforeDestroy () {
+    console.log('defeated!!!')
+    clearInterval(this.timer)
+  },
+  created () {
+    this.timer = setInterval(() => {
+      this.position.x += 0.2
+    }, constants.FRAME_RATE / 2)
+  }
 }
 </script>
 
